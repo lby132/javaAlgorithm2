b2116b14b8b603f7c749ec4534e919c9c4ce8c83
lby132 <quddbscjswow@naver.com>
2023-04-21 01:26:11 +0900

    solution

--- src/string/FlipWord.java
+++ src/string/FlipWord.java
@@ -1,43 +1,93 @@
 package string;
 
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class FlipWord {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
 
-        int num = sc.nextInt();
-        int a = 0;
+//    설명
+//
+//    N개의 단어가 주어지면 각 단어를 뒤집어 출력하는 프로그램을 작성하세요.
 
-        while (sc.hasNext()) {
-            String str = sc.next();
+    public ArrayList<String> solution(int n, String[] str) {
 
-            Flip(Arrays.asList(str));
+        ArrayList<String> answer = new ArrayList<>();
 
-            a++;
-            if (a == num) break;
-        }
-
-    }
-
-    public static String Flip(List<String> str) {
-
-        String[] strArr;
         for (String s : str) {
-            strArr = s.split("");
-            System.out.println("strArr.length = " + strArr.length);
-            for (int i = 0; i < s.length(); i++) {
-                System.out.println("strArr = " + strArr[i]);
-                //Comparator 메소드 사용하기
-            }
-
+            String tmp = new StringBuilder(s).reverse().toString();
+            answer.add(tmp);
         }
 
-
-        return null;
+        return answer;
     }
+
+    public ArrayList<String> solution1(int n, String[] str) {
+
+        ArrayList<String> answer = new ArrayList<>();
+
+        for (String x : str) {
+            char[] s = x.toCharArray();
+            int lt = 0, rt = x.length() - 1;
+            while (lt < rt) {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
+                lt++;
+                rt--;
+            }
+            String tmp = String.valueOf(s);
+            answer.add(tmp);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        FlipWord t = new FlipWord();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        String[] str = new String[n];
+        for (int i = 0; i < n; i++) {
+            str[i] = kb.next();
+        }
+        for (String x : t.solution1(n, str)) {
+            System.out.println(x);
+        }
+    }
+
+
+
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        int num = sc.nextInt();
+//        int a = 0;
+//
+//        while (sc.hasNext()) {
+//            String str = sc.next();
+//
+//            System.out.println(Flip(Arrays.asList(str)));
+//
+//            a++;
+//            if (a == num) break;
+//        }
+//    }
+//
+//    public static String Flip(List<String> str) {
+//
+//        Collections.reverse(str);
+//
+//        String[] strArr;
+//        String answer = "";
+//        for (String s : str) {
+//            strArr = s.split("");
+//            for (int i = s.length() - 1; i > -1; i--) {
+//                answer += strArr[i];
+//            }
+//
+//        }
+//
+//        return answer;
+//    }
 
 
 }
--- src/string/LongWord.java
+++ src/string/LongWord.java
@@ -5,6 +5,9 @@ import java.util.Scanner;
 
 public class LongWord {
 
+    // 한 개의 문장이 주어지면 그 문장 속에서 가장 긴 단어를 출력하는 프로그램을 작성하세요.
+    // 문장속의 각 단어는 공백으로 구분됩니다.
+
     public String solution1(String str) {
         String answer = "";
         int m = Integer.MIN_VALUE, pos;
