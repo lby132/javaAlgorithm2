d5033cc354853d75986275c8ca945ac64eeeec84
lby132 <quddbscjswow@naver.com>
2023-04-25 00:01:45 +0900

    solution

--- src/string/ReverseChar.java
+++ src/string/ReverseChar.java
@@ -11,55 +11,43 @@ public class ReverseChar {
 //    영어 알파벳과 특수문자로 구성된 문자열이 주어지면 영어 알파벳만 뒤집고,
 //    특수문자는 자기 자리에 그대로 있는 문자열을 만들어 출력하는 프로그램을 작성하세요.
 
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str = sc.next();
 
 
-        reverseChar(str);
+        System.out.println(reverseChar(str));
     }
 
     public static String reverseChar(String str) {
 
-        List<String> list = new ArrayList<>();
+        String s = "";
         for (int i = 0; i < str.length(); i++) {
             char[] c = str.toCharArray();
             int lt = 0, rt = str.length() - 1;
-            boolean numeric = isNumeric(c);
-            System.out.println("numeric = " + numeric);
-
             while (lt < rt) {
+                char tmp = c[lt];
+                if (!(c[lt] >= 'a' && c[lt] <= 'z') && !(c[lt] >= 'A' && c[lt] <= 'Z')) {
 
-                if (numeric) {
-                    System.out.println("numeric = " + numeric);
-                    char tmp = c[lt];
-                    c[lt] = c[rt];
-                    c[rt] = tmp;
-                    lt++;
-                    rt--;
                 } else {
-                    System.out.println(" =sdfsd " );
-                    continue;
+                                        c[lt] = c[rt];
+
                 }
-            }
-            System.out.println("Arrays.toString(c) = " + Arrays.toString(c));
+                if (!(c[rt] >= 'A' && c[rt] <= 'Z') && !(c[rt] >= 'a' && c[rt] <= 'z')) {
+                } else {
+                    c[rt] = tmp;
 
+                }
+                lt++;
+                rt--;
+            }
+            s = String.valueOf(c);
         }
-        return "";
+
+        return s;
 
     }
 
-    public static boolean isNumeric(char[] c) {
-        for (char c1 : c) {
-            if (!(c1 >= 'A' && c1 <= 'Z') && !(c1 >= 'a' && c1 <= 'z')) {
-                return false;
-
-            }
-            continue;
-        }
-        return true;
-    }
 }
 
 
