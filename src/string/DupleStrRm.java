4fb37f022ed9be59340c71b7b9402552f5d0de9f
lby132 <quddbscjswow@naver.com>
2023-04-26 00:11:29 +0900

    solution

---
+++ src/string/DupleStrRm.java
@@ -0,0 +1,31 @@
+package string;
+
+import java.util.Scanner;
+
+public class DupleStrRm {
+
+//    설명
+//
+//    소문자로 된 한개의 문자열이 입력되면 중복된 문자를 제거하고 출력하는 프로그램을 작성하세요.
+//
+//    중복이 제거된 문자열의 각 문자는 원래 문자열의 순서를 유지합니다.
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(solution(str));
+
+    }
+
+    public static String solution(String str) {
+
+        String answer = "";
+        for (int i = 0; i < str.length(); i++) {
+            if (str.indexOf(str.charAt(i)) == i) {
+                answer += str.charAt(i);
+            }
+        }
+
+        return answer;
+    }
+}
