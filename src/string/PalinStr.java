445c547f3a5bc674b56ca394e76e484cff2a8bbf
lby132 <quddbscjswow@naver.com>
2023-04-28 00:24:09 +0900

    solution

--- src/string/PalinStr.java
+++ src/string/PalinStr.java
@@ -6,10 +6,9 @@ import java.util.Scanner;
 public class PalinStr {
 //    설명
 
-//    앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 회문 문자열이라고 합니다.
+    //    앞에서 읽을 때나 뒤에서 읽을 때나 같은 문자열을 회문 문자열이라고 합니다.
 //    문자열이 입력되면 해당 문자열이 회문 문자열이면 "YES", 회문 문자열이 아니면 “NO"를 출력하는 프로그램을 작성하세요.
 //    단 회문을 검사할 때 대소문자를 구분하지 않습니다.
-
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         String str = kb.next();
@@ -18,13 +17,38 @@ public class PalinStr {
 
     public static String solution(String str) {
 
-        String answer;
-        StringBuffer sb = new StringBuffer(str);
-        sb.reverse();
-
-        if (str.compareToIgnoreCase(sb.toString()) == 0) answer = "YES";
-        else answer = "NO";
+        String answer = "YES";
+        int len = str.length();
+        for (int i = 0; i < len/2; i++) {
+            if (str.charAt(i) != str.charAt(len-i-1)) answer = "NO";
+        }
 
         return answer;
     }
+
+    public static String solution2(String str) {
+
+        String answer = "NO";
+        String tmp = new StringBuilder(str).reverse().toString();
+        if (str.equalsIgnoreCase(tmp)) answer = "YES";
+        return answer;
+    }
+
+//    public static void main(String[] args) {
+//        Scanner kb = new Scanner(System.in);
+//        String str = kb.next();
+//        System.out.println(solution(str));
+//    }
+//
+//    public static String solution(String str) {
+//
+//        String answer;
+//        StringBuffer sb = new StringBuffer(str);
+//        sb.reverse();
+//
+//        if (str.compareToIgnoreCase(sb.toString()) == 0) answer = "YES";
+//        else answer = "NO";
+//
+//        return answer;
+//    }
 }
--- src/string/ReverseChar.java
+++ src/string/ReverseChar.java
@@ -10,43 +10,66 @@ public class ReverseChar {
 //
 //    영어 알파벳과 특수문자로 구성된 문자열이 주어지면 영어 알파벳만 뒤집고,
 //    특수문자는 자기 자리에 그대로 있는 문자열을 만들어 출력하는 프로그램을 작성하세요.
+//    예시 a#b!GE*T@S
 
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String str = sc.next();
-
-
-        System.out.println(reverseChar(str));
-    }
-
-    public static String reverseChar(String str) {
-
-        String s = "";
-        for (int i = 0; i < str.length(); i++) {
-            char[] c = str.toCharArray();
-            int lt = 0, rt = str.length() - 1;
-            while (lt < rt) {
-                char tmp = c[lt];
-                if (!(c[lt] >= 'a' && c[lt] <= 'z') && !(c[lt] >= 'A' && c[lt] <= 'Z')) {
-
-                } else {
-                                        c[lt] = c[rt];
-
-                }
-                if (!(c[rt] >= 'A' && c[rt] <= 'Z') && !(c[rt] >= 'a' && c[rt] <= 'z')) {
-                } else {
-                    c[rt] = tmp;
-
-                }
+    public static String solution(String str) {
+        String answer;
+        char[] s = str.toCharArray();
+        int lt = 0, rt = str.length() - 1;
+        while (lt < rt) {
+            if (!Character.isAlphabetic(s[lt])) lt++;
+            else if (!Character.isAlphabetic(s[rt])) rt--;
+            else {
+                char tmp = s[lt];
+                s[lt] = s[rt];
+                s[rt] = tmp;
                 lt++;
                 rt--;
             }
-            s = String.valueOf(c);
         }
-
-        return s;
-
+        answer = String.valueOf(s);
+        return answer;
     }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(solution(str));
+    }
+
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.next();
+//
+//
+//        System.out.println(reverseChar(str));
+//    }
+//
+//    public static String reverseChar(String str) {
+//
+//        String s = "";
+//        for (int i = 0; i < str.length(); i++) {
+//            char[] c = str.toCharArray();
+//            int lt = 0, rt = str.length() - 1;
+//            while (lt < rt) {
+//                char tmp = c[lt];
+//                if (!(c[lt] >= 'a' && c[lt] <= 'z') && !(c[lt] >= 'A' && c[lt] <= 'Z')) {
+//                } else {
+//                    c[lt] = c[rt];
+//                }
+//                if (!(c[rt] >= 'A' && c[rt] <= 'Z') && !(c[rt] >= 'a' && c[rt] <= 'z')) {
+//                } else {
+//                    c[rt] = tmp;
+//                }
+//                lt++;
+//                rt--;
+//            }
+//            s = String.valueOf(c);
+//        }
+//
+//        return s;
+//
+//    }
 
 }
 
