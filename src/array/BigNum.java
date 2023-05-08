b4f0205cc4dc41a054540bc5eb1e661a95b3486d
lby132 <quddbscjswow@naver.com>
2023-05-09 00:31:28 +0900

    solution

---
+++ src/array/BigNum.java
@@ -0,0 +1,66 @@
+package array;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class BigNum {
+
+//    설명
+//
+//    N개의 정수를 입력받아, 자신의 바로 앞 수보다 큰 수만 출력하는 프로그램을 작성하세요.
+//
+//    (첫 번째 수는 무조건 출력한다)
+
+
+    public static ArrayList<Integer> solution(int n, int[] arr) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        answer.add(arr[0]);
+        for (int i = 1; i < n; i++) {
+            if (arr[i] > arr[i - 1]) answer.add(arr[i]);
+        }
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for (int x : solution(n, arr)) {
+            System.out.println(x + " ");
+        }
+    }
+
+//    public static void main(String[] args) {
+//        Scanner kb = new Scanner(System.in);
+//        int num = kb.nextInt();
+//        List<Integer> numbers = new ArrayList();
+//        for (int i = 0; i < num; i++) {
+//            numbers.add(kb.nextInt());
+//        }
+//
+//        System.out.println(solution(numbers, num));
+//    }
+//
+//    public static String solution(List<Integer> numbers, int num) {
+//
+//        List<Integer> arr = new ArrayList();
+//        for (int number : numbers) {
+//            if (number >= num) {
+//                arr.add(number);
+//            }
+//        }
+//        String answer = "";
+//        for (Integer integer : arr) {
+//            answer += integer + " ";
+//        }
+//
+//        return answer;
+//    }
+}
+
+//7 3 9 5 6 12
---
+++ src/array/HeightStudent.java
@@ -0,0 +1,37 @@
+package array;
+
+import java.util.Scanner;
+
+public class HeightStudent {
+
+//    설명
+//
+//    선생님이 N명의 학생을 일렬로 세웠습니다. 일렬로 서 있는 학생의 키가 앞에서부터 순서대로 주어질 때, 맨 앞에 서 있는
+//
+//    선생님이 볼 수 있는 학생의 수를 구하는 프로그램을 작성하세요. (앞에 서 있는 사람들보다 크면 보이고, 작거나 같으면 보이지 않습니다.)
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+
+        System.out.println(solution(n, arr));
+    }
+
+
+    public static int solution(int n, int[] arr) {
+        int answer = 1, max = arr[0];
+        for (int i = 0; i < n; i++) {
+            if (arr[i] > max) {
+                answer++;
+                max = arr[i];
+            }
+        }
+
+        return answer;
+    }
+
+}
