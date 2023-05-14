f96de686cea1582e8be90dbe9f999cf9c97e68dc
lby132 <quddbscjswow@naver.com>
2023-05-15 00:13:19 +0900

    solution

--- src/array/HeightStudent.java
+++ src/array/HeightStudent.java
@@ -10,6 +10,7 @@ public class HeightStudent {
 //
 //    선생님이 볼 수 있는 학생의 수를 구하는 프로그램을 작성하세요. (앞에 서 있는 사람들보다 크면 보이고, 작거나 같으면 보이지 않습니다.)
 
+
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
         int n = kb.nextInt();
