eb1875de4becc39dc6cceb2c9280743c8410c2ac
lby132 <quddbscjswow@naver.com>
2023-05-17 21:43:20 +0900

    solution

---
+++ src/array/Rank.java
@@ -0,0 +1,37 @@
+package array;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Rank {
+
+//    설명
+//
+//    N명의 학생의 국어점수가 입력되면 각 학생의 등수를 입력된 순서대로 출력하는 프로그램을 작성하세요.
+//    같은 점수가 입력될 경우 높은 등수로 동일 처리한다.
+//    즉 가장 높은 점수가 92점인데 92점이 3명 존재하면 1등이 3명이고 그 다음 학생은 4등이 된다.
+
+    public static int[] solution(int n, int[] arr) {
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
+            int cnt = 1;
+            for (int j = 0; j < n; j++) {
+                if (arr[j] > arr[i]) cnt++;
+            }
+            answer[i] = cnt;
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        for (int x : solution(n, arr)) System.out.print(x + " ");
+
+    }
+
+}
