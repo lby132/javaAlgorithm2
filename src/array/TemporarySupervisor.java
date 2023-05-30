1ecdd08dbef5abb04aef4122b6d2c06d15525014
lby132 <quddbscjswow@naver.com>
2023-05-30 21:24:21 +0900

    solution

---
+++ src/array/TemporarySupervisor.java
@@ -0,0 +1,41 @@
+package array;
+
+import java.util.Scanner;
+
+public class TemporarySupervisor {
+
+    //11. 임시반장 정하기
+    public int solution(int n, int[][] arr) {
+        int answer = 0, max = Integer.MIN_VALUE;
+        for (int i = 1; i <= n; i++) {
+            int cnt = 0;
+            for (int j = 1; j <= n; j++) {
+                for (int k = 1; k <= 5; k++) {
+                    if (arr[i][k] == arr[j][k]) {
+                        cnt++;
+                        break;
+                    }
+                }
+            }
+            if (cnt > max) {
+                max = cnt;
+                answer=i;
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        TemporarySupervisor t = new TemporarySupervisor();
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[][] arr = new int[n + 1][6];
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= 5; j++) {
+                arr[i][j] = kb.nextInt();
+            }
+        }
+        System.out.print(t.solution(n, arr));
+    }
+}
