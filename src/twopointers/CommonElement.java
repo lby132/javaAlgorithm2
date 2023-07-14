fe8bc083320160eeb583db331e4709d7eb0959a4
lby132 <quddbscjswow@naver.com>
2023-07-15 00:09:21 +0900

    solution

--- src/twopointers/CommonElement.java
+++ src/twopointers/CommonElement.java
@@ -1,36 +1,38 @@
-package array;
+package twopointers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Scanner;
 
-public class TwoArrayCombine {
+public class CommonElement {
 
 /*
-    1. 두 배열 합치기
+    설명
 
-    오름차순으로 정렬이 된 두 배열이 주어지면 두 배열을 오름차순으로 합쳐 출력하는 프로그램을 작성하세요.
+    A, B 두 개의 집합이 주어지면 두 집합의 공통 원소를 추출하여 오름차순으로 출력하는 프로그램을 작성하세요.
+
 */
-
-
 
 
 
     public static ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
         ArrayList<Integer> answer = new ArrayList<>();
-        int p1 = 0, p2 = 0;
+        Arrays.sort(a);
+        Arrays.sort(b);
+        int p1=0, p2=0;
         while (p1 < n && p2 < m) {
-            if (a[p1] < b[p2]) {
+            if (a[p1] == b[p2]) {
                 answer.add(a[p1++]);
+                p2++;
+            } else if (a[p1] < b[p2]) {
+                p1++;
             } else {
-                answer.add(b[p2++]);
+                p2++;
             }
         }
-        while (p1 < n) answer.add(a[p1++]);
-        while (p2 < m) answer.add(a[p2++]);
 
         return answer;
     }
-
 
     public static void main(String[] args) {
         Scanner kb = new Scanner(System.in);
@@ -45,7 +47,7 @@ public class TwoArrayCombine {
             b[i] = kb.nextInt();
         }
         for (int x : solution(n, m, a, b)) {
-            System.out.println(x+" ");
+            System.out.println(x + " ");
         }
     }
 }
---
+++ src/twopointers/TwoArrayCombine.java
@@ -0,0 +1,53 @@
+package twopointers;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class TwoArrayCombine {
+
+/*
+    설명
+
+    1. 두 배열 합치기
+
+    오름차순으로 정렬이 된 두 배열이 주어지면 두 배열을 오름차순으로 합쳐 출력하는 프로그램을 작성하세요.
+*/
+
+
+
+
+
+    public static ArrayList<Integer> solution(int n, int m, int[] a, int[] b) {
+        ArrayList<Integer> answer = new ArrayList<>();
+        int p1 = 0, p2 = 0;
+        while (p1 < n && p2 < m) {
+            if (a[p1] < b[p2]) {
+                answer.add(a[p1++]);
+            } else {
+                answer.add(b[p2++]);
+            }
+        }
+        while (p1 < n) answer.add(a[p1++]);
+        while (p2 < m) answer.add(a[p2++]);
+
+        return answer;
+    }
+
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = kb.nextInt();
+        }
+        int m = kb.nextInt();
+        int[] b = new int[m];
+        for (int i = 0; i < m; i++) {
+            b[i] = kb.nextInt();
+        }
+        for (int x : solution(n, m, a, b)) {
+            System.out.println(x+" ");
+        }
+    }
+}
