6b63c522b78456cc2830bfae136adb55094251c1
lby132 <quddbscjswow@naver.com>
2023-07-16 00:53:15 +0900

    solution

---
+++ src/twopointers/MaximumSales.java
@@ -0,0 +1,30 @@
+package twopointers;
+
+import java.util.Scanner;
+
+public class MaximumSales {
+
+    public static int solution(int n, int k, int[] arr) {
+        int answer = 0, sum = 0;
+        for (int i = 0; i < k; i++) {
+            sum += arr[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            sum += (arr[i] - arr[i - k]);
+            answer = Math.max(answer, sum);
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int k = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.println(solution(n, k, arr));
+    }
+}
