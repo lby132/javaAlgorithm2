d3404ac7729d55d013befda6563f8490619accfe
lby132 <quddbscjswow@naver.com>
2023-07-17 00:09:51 +0900

    solution

--- src/twopointers/MaximumSales.java
+++ src/twopointers/MaximumSales.java
@@ -4,6 +4,18 @@ import java.util.Scanner;
 
 public class MaximumSales {
 
+/*    설명
+
+    현수의 아빠는 제과점을 운영합니다. 현수 아빠는 현수에게 N일 동안의 매출기록을 주고 연속된 K일 동안의 최대 매출액이 얼마인지 구하라고 했습니다.
+
+    만약 N=10이고 10일 간의 매출기록이 아래와 같습니다. 이때 K=3이면
+
+    12 1511 20 2510 20 19 13 15
+
+    연속된 3일간의 최대 매출액은 11+20+25=56만원입니다.
+
+    여러분이 현수를 도와주세요.*/
+
     public static int solution(int n, int k, int[] arr) {
         int answer = 0, sum = 0;
         for (int i = 0; i < k; i++) {
