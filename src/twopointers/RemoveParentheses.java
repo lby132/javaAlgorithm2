e6d112a31124dc077701fa289acfb64917b50fe5
lby132 <quddbscjswow@naver.com>
2023-07-27 23:10:02 +0900

    solution

---
+++ src/twopointers/RemoveParentheses.java
@@ -0,0 +1,30 @@
+package twopointers;
+
+import java.util.Scanner;
+import java.util.Stack;
+
+public class RemoveParentheses {
+
+    public static String solution(String str) {
+        String answer = "";
+        Stack<Character> stack = new Stack<>();
+        for (char x : str.toCharArray()) {
+            if (x == ')') {
+                while (stack.pop() != '(');
+            } else {
+                stack.push(x);
+            }
+        }
+        for (int i = 0; i < stack.size(); i++) {
+            answer += stack.get(i);
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        String str = kb.next();
+        System.out.println(solution(str));
+    }
+}
