5f36015bb23f9b49304e9098b4d10dcd885e2ce8
lby132 <quddbscjswow@naver.com>
2023-08-03 16:02:28 +0900

    solution

---
+++ src/sortingandsearch/SelectionSort.java
@@ -0,0 +1,33 @@
+package sortingandsearch;
+
+import java.util.Scanner;
+
+public class SelectionSort {
+
+    public static int[] solution(int n, int[] arr) {
+        for (int i = 0; i < n - 1; i++) {
+            int idx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[idx]) {
+                    idx = j;
+                }
+            }
+            int tmp = arr[i];
+            arr[i] = arr[idx];
+            arr[idx] = tmp;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        for (int x : solution(n, arr)) {
+            System.out.print(x + " ");
+        }
+    }
+}
