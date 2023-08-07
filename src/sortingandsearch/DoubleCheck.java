3b30690e23e6151645877291f0b9963aff1b9b9e
lby132 <quddbscjswow@naver.com>
2023-08-07 20:35:05 +0900

    solution

---
+++ src/sortingandsearch/DoubleCheck.java
@@ -0,0 +1,29 @@
+package sortingandsearch;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class DoubleCheck {
+
+    public static String solution(int n, int[] arr) {
+        String answer = "U";
+        Arrays.sort(arr);
+        for (int i = 0; i < n - 1; i++) {
+            if (arr[i] == arr[i + 1]) {
+                return "D";
+            }
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        Scanner kb = new Scanner(System.in);
+        int n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        System.out.println(solution(n, arr));
+    }
+}
