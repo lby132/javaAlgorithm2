6c6fb301a47adf17027a8e58a4800e29b3bacabd
lby132 <quddbscjswow@naver.com>
2023-08-16 19:58:20 +0900

    solution

---
+++ src/recursivetreegraph/FibonacciSequence.java
@@ -0,0 +1,28 @@
+package recursivetreegraph;
+
+public class FibonacciSequence {
+
+    static int[] fibo;
+
+    public static int DFS(int n) {
+        if (fibo[n] > 0) {
+            return fibo[n];
+        }
+        if (n == 1) {
+            return fibo[n] = 1;
+        } else if (n == 2) {
+            return fibo[n] = 1;
+        } else {
+            return fibo[n] = DFS(n - 2) + DFS(n - 1);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 45;
+        fibo = new int[n + 1];
+        DFS(n);
+        for (int i = 0; i <= n; i++) {
+            System.out.println(fibo[i] + " ");
+        }
+    }
+}
