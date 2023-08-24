8edccb5c5eb66f75d1fa04a8bb4a87f66720a92c
lby132 <quddbscjswow@naver.com>
2023-08-24 13:29:50 +0900

    solution

---
+++ src/recursivetreegraph/AdjacencyList.java
@@ -0,0 +1,67 @@
+package recursivetreegraph;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class AdjacencyList {
+
+    // 가지수
+
+    //1 2 3 4 5
+    //1 2 5
+    //1 3 4 2 5
+    //1 3 4 5
+    //1 4 2 5
+    //1 4 5
+
+    static int n, m, answer = 0;
+    static ArrayList<ArrayList<Integer>> graph;
+    static int[] ch;
+
+    public void DFS(int v) {
+        if (v == n) {
+            answer++;
+        } else {
+            for (int nv : graph.get(v)) {
+                if (ch[nv] == 0) {
+                    ch[nv] = 1;
+                    DFS(nv);
+                    ch[nv] = 0;
+                }
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        AdjacencyList T = new AdjacencyList();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        m = kb.nextInt();
+        graph = new ArrayList<ArrayList<Integer>>();
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<Integer>());
+        }
+        ch = new int[n + 1];
+        for (int i = 0; i < m; i++) {
+            int a = kb.nextInt();
+            int b = kb.nextInt();
+            graph.get(a).add(b);
+        }
+        ch[1] = 1;
+        T.DFS(1);
+        System.out.println(answer);
+
+    }
+}
+
+
+//5 9
+//1 2
+//1 3
+//1 4
+//2 1
+//2 3
+//2 5
+//3 4
+//4 2
+4 5
