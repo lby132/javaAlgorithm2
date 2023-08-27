84df4efef64c400d637941fc6130f581db56981d
lby132 <quddbscjswow@naver.com>
2023-08-27 12:37:55 +0900

    solution

---
+++ src/recursivetreegraph/GoRiding.java
@@ -0,0 +1,31 @@
+package recursivetreegraph;
+
+import java.util.Scanner;
+
+public class GoRiding {
+
+    static int answer = Integer.MIN_VALUE, c, n;
+
+    public void DFS(int L, int sum, int[] arr) {
+        if (sum > c) return;
+        if (L == n) {
+            answer = Math.max(answer, sum);
+        } else {
+            DFS(L + 1, sum + arr[L], arr);
+            DFS(L + 1, sum, arr);
+        }
+    }
+
+    public static void main(String[] args) {
+        GoRiding T = new GoRiding();
+        Scanner kb = new Scanner(System.in);
+        c = kb.nextInt();
+        n = kb.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = kb.nextInt();
+        }
+        T.DFS(0, 0, arr);
+        System.out.println(answer);
+    }
+}
