a91922c493a607e0b353619f434e6556d1c889d4
lby132 <quddbscjswow@naver.com>
2023-08-29 16:50:51 +0900

    solution

---
+++ src/recursivetreegraph/DoublePermutation.java
@@ -0,0 +1,34 @@
+package recursivetreegraph;
+
+import java.util.Scanner;
+
+public class DoublePermutation {
+
+    static int[] pm;
+    static int n, m;
+
+    public void DFS(int L) {
+        if (L == m) {
+            for (int x : pm) {
+                System.out.print(x + " ");
+                System.out.println();
+            }
+        } else {
+            for (int i = 1; i <= n; i++) {
+                pm[L] = i;
+                DFS(L + 1);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        DoublePermutation T = new DoublePermutation();
+        Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        m = kb.nextInt();
+        pm = new int[m];
+        T.DFS(0);
+    }
+}
+
+
