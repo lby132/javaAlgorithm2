9230409ea03206ba17de3489549e0da722111908
lby132 <quddbscjswow@naver.com>
2023-09-15 16:35:42 +0900

    solution

---
+++ src/greedyalorithm/MaximumIncomeSchedule.java
@@ -0,0 +1,53 @@
+package greedyalorithm;
+
+import java.util.*;
+
+class Lecture implements Comparable<Lecture> {
+
+    int money;
+    int time;
+
+    public Lecture(int money, int time) {
+        this.money = money;
+        this.time = time;
+    }
+
+    @Override
+    public int compareTo(Lecture o) {
+        return o.time - this.time;
+    }
+}
+
+public class MaximumIncomeSchedule {
+
+    static int n, max = Integer.MIN_VALUE;
+
+    public int solution(ArrayList<Lecture> arr){
+        int answer=0;
+        PriorityQueue<Integer> pQ = new PriorityQueue<>(Collections.reverseOrder());
+        Collections.sort(arr);
+        int j=0;
+        for(int i=max; i>=1; i--){
+            for(; j<n; j++){
+                if(arr.get(j).time<i) break;
+                pQ.offer(arr.get(j).money);
+            }
+            if(!pQ.isEmpty()) answer+=pQ.poll();
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        final MaximumIncomeSchedule T = new MaximumIncomeSchedule();
+        final Scanner kb = new Scanner(System.in);
+        n = kb.nextInt();
+        ArrayList<Lecture> arr = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            int m = kb.nextInt();
+            int d = kb.nextInt();
+            arr.add(new Lecture(m, d));
+            if (d > max) max = d;
+        }
+        System.out.println(T.solution(arr));
+    }
+}
